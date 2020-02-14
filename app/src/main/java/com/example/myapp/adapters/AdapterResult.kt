29a6ce21d51ac77aa746e1e5a58a4436a8dfa034
Gillam Hazrak zo loraq <ghazrak-@oa-m5.21-school.ru>
2020-02-14 17:36:54 +0300

    add design for result activity

--- app/src/main/java/com/example/myapp/adapters/AdapterResult.kt
+++ app/src/main/java/com/example/myapp/adapters/AdapterResult.kt
@@ -9,7 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.myapp.R
 import com.example.myapp.model.User
 
-class AdapterResult(private var users : ArrayList<User>, val listener:(Int) -> Unit) : RecyclerView.Adapter<AdapterResult.ViewHolder>() {
+class AdapterResult(private var users : ArrayList<User>, val listener:(Int) -> Unit, val listenerButton:(Int) -> Unit) : RecyclerView.Adapter<AdapterResult.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val itemView =
             LayoutInflater.from(parent.context).inflate(R.layout.list_result, parent, false)
@@ -28,13 +28,21 @@ class AdapterResult(private var users : ArrayList<User>, val listener:(Int) -> U
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var name : TextView = itemView.findViewById(R.id.name_result)
         var price : TextView = itemView.findViewById(R.id.money_result)
+        var price2 : TextView = itemView.findViewById(R.id.tv)
+        var price3 : TextView = itemView.findViewById(R.id.tv2)
         var share : Button = itemView.findViewById(R.id.share_button)
+        var edit : Button = itemView.findViewById(R.id.edit_button)
 
         fun bind(pos : Int) {
             name.text = users[pos].name
-            price.text = (users[pos].money.toString().toDouble() / 100).toString()
+            price.text = "Итого:  " + (users[pos].money.toString().toDouble() / 100).toString()
+            price3.text = "Осталось заплатить:  " + (users[pos].money.toString().toDouble() / 100).toString()
+            price2.text = "Заплачено:  0.00"
             share.setOnClickListener {
                 listener(pos)
+            }
+            edit.setOnClickListener {
+                listenerButton(pos)
             }
 
         }
--- app/src/main/java/com/example/myapp/view/ResultActivity.kt
+++ app/src/main/java/com/example/myapp/view/ResultActivity.kt
@@ -4,7 +4,9 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.LinearLayout
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapp.PresenterHolder
 import com.example.myapp.adapters.AdapterResult
@@ -14,6 +16,7 @@ import com.example.myapp.R
 import com.example.myapp.model.Model
 import com.example.myapp.model.User
 import kotlinx.android.synthetic.main.activity_result.*
+import kotlinx.android.synthetic.main.edit_dialog.view.*
 
 class ResultActivity : AppCompatActivity(), ResultContract.view {
 
@@ -38,16 +41,44 @@ class ResultActivity : AppCompatActivity(), ResultContract.view {
 
     fun showUsers(array : ArrayList<User>) {
         users = array
-        recycleResult.adapter = AdapterResult(users) {
-            val sendIntent: Intent = Intent().apply {
-                action = Intent.ACTION_SEND
-                putExtra(Intent.EXTRA_TEXT, "Hi, you owe me ${users[it].money}")
-                type = "text/plain"
-            }
+        recycleResult.adapter = AdapterResult(users, listener = {listener(it)}, listenerButton = {listenerButton(it)})
+    }
 
-            val shareIntent = Intent.createChooser(sendIntent, null)
-            startActivity(shareIntent)
+    private fun listenerButton(id : Int) {
+
+        val mDialogView = LayoutInflater.from(this).inflate(R.layout.edit_dialog, null)
+        //AlertDialogBuilder
+        val mBuilder = AlertDialog.Builder(this)
+            .setView(mDialogView)
+        //show dialog
+        val  mAlertDialog = mBuilder.show()
+        //login button click of custom layout
+        mDialogView.ok_button.setOnClickListener {
+            //dismiss dialog
+            mAlertDialog.dismiss()
+            //get text from EditTexts of custom layout
+            val name = mDialogView.edit.text.toString()
+            //arr[id] = Triple(arr[id].first, name, arr[id].third)
+
+            //adapter.notifyDataSetChanged()
+            //set the input text in TextView
         }
+        //cancel button click of custom layout
+        mDialogView.cancel_button.setOnClickListener {
+            //dismiss dialog
+            mAlertDialog.dismiss()
+        }
+    }
+
+    private fun listener(id : Int) {
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, "Hi, you owe me ${users[id].money}")
+            type = "text/plain"
+        }
+
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        startActivity(shareIntent)
     }
 
     override fun onStop() {
