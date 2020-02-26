f7b8cb52cbe320aee5927fc9b8b0f36301eec3d6
SillyDreamer <L1sen0k1@yandex.ru>
2020-02-27 02:53:19 +0300

    added few ui tests

---
+++ app/src/androidTest/java/com/example/myapp/view/AddUserActivityTest.kt
@@ -0,0 +1,24 @@
+package com.example.myapp.view
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.rule.ActivityTestRule
+import com.example.myapp.R
+import org.junit.Rule
+import org.junit.Test
+
+class AddUserActivityTest {
+
+
+    @get:Rule
+    val activityRule = ActivityTestRule(AddUserActivity::class.java)
+
+    @Test
+    fun test() {
+        onView(withId(R.id.editText)).perform(typeText("hello"))
+        onView(withId(R.id.add)).perform(click())
+        onView(withId(R.id.button_next)).perform(click())
+    }
+}
--- app/src/androidTest/java/com/example/myapp/view/PreviousCheckActivityTest.kt
+++ app/src/androidTest/java/com/example/myapp/view/PreviousCheckActivityTest.kt
@@ -1,7 +1,28 @@
 package com.example.myapp.view
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import com.example.myapp.R
 import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
+@LargeTest
 class PreviousCheckActivityTest {
+
+    @get:Rule
+    val activityRule = ActivityTestRule(PreviousCheckActivity::class.java)
+
+    @Test
+    fun changeText_sameActivity() {
+        // Type text and then press the button.
+        onView(withId(R.id.fab))
+    }
 
 }
